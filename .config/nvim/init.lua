38fa0cf27cf742fc34dd277b6397a86baef80681
Natã Estanislau Bastos <nataestanislaubastos@gmail.com>
2023-03-28 11:54:07 -0300

    moving to nvchad

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,56 +1,22 @@
-require("plugins")
+require "core"
 
-vim.g.mapleader = " "
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set number")
-vim.cmd("set rnu")
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
-vim.cmd([[
-autocmd BufRead,BufEnter *.astro set filetype=astro
-]])
+require("core.utils").load_mappings()
 
-vim.api.nvim_set_keymap("n", "<space>fr", ":Telescope file_browser<CR>", { noremap = true })
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-vim.api.nvim_set_keymap("n", "<space>fe", ":Telescope file_browser path=%:p:h<CR>", { noremap = true })
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
 
-vim.api.nvim_set_keymap("n", "<space>gc", ":Neogit commit<CR>", { noremap = true })
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
 
-vim.api.nvim_set_keymap("n", "<space>gs", ":Telescope git_status<CR>", { noremap = true })
-
-vim.api.nvim_set_keymap("n", "<space>gs", ":Telescope git_status<CR>", { noremap = true })
-
-local wk = require("which-key")
-
-wk.register({
-	e = { "<cmd>Neotree<cr>", "Open FileTree" },
-	l = {
-		name = "LSP",
-		f = {
-			function()
-				vim.lsp.buf.format({
-					filter = function(client)
-						return client.name ~= "tsserver"
-					end,
-				})
-			end,
-			"Format buffer",
-		},
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Actions" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-	},
-}, { prefix = "<leader>" })
-
-wk.register({
-	K = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover" },
-}, { mode = "n" })
-
-wk.register({
-	d = { "<cmd>Telescope lsp_definitions<cr>", "Definition" },
-	r = { "<cmd>Telescope lsp_references<cr>", "References" },
-	s = { "<cmd>Telescope lsp_document_symbols<cr>", "Symbols" },
-	i = { "<cmd>Telescope lsp_implementations<cr>", "Implementations" },
-	t = { "<cmd>Telescope lsp_type_definitions<cr>", "Type Definitions" },
-}, { prefix = "g" })
-
-wk.setup({})
+dofile(vim.g.base46_cache .. "defaults")
